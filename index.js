0ae4046fa80b379090a43491561d5c5d98c7c95a
tarun <tk4977163@gmail.com>
2021-06-19 00:37:09 +0530

    UI complete working on Interactivity

---
+++ index.js
@@ -0,0 +1,44 @@
+//Using Jquery for Dealing with DOM
+
+$(document).ready(() => {
+  //For column name code and row
+  for (let i = 1; i <= 100; i++) {
+    let ans = "";
+    let n = i;
+    while (n) {
+      n -= 1; //0 means A
+      let rem = n % 26;
+      ans = String.fromCharCode(rem + 65) + ans;
+      n = Math.floor(n / 26);
+    }
+    $(".column_name_container").append(
+      `<div class="column_name colID-${i}" id="colCod-${ans}">${ans}</div>`
+    );
+    $(".row_name_container").append(
+      `<div class="row_name rowID-${i}" id="rowCod-${i}">${i}</div>`
+    );
+  }
+  //Adding cells
+
+  for (let i = 1; i <= 100; i++) {
+    let row = $(`<div class="cell_row"></div>`);
+    for (let j = 1; j <= 100; j++) {
+      let colCod = $(`.colID-${j}`).attr("id").split("-")[1];
+      row.append(
+        `<div class="input_cell row-${i}-col-${j}" contenteditable="true" data="colCod-${colCod}"></div>`
+      );
+    }
+    $(".input_container").append(row);
+  }
+
+  //SELECTED item
+
+  $(".menu-icon.align-icon").click(() => {
+    $(".menu-icon.align-icon.selected").removeClass("selected"); //Removing class form the div which already have
+    $(this).addClass("selected"); //Adding class of to the clicked align icon
+  });
+
+  $(".style-icon").click(function () {
+    $(this).toggleClass("selected");
+  });
+});
