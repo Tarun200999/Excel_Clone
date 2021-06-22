49e57f4810781d46c1c24fd0a7bd365e093db57e
tarun <tk4977163@gmail.com>
2021-06-22 17:18:39 +0530

    Multiple Cell Select Done

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ $(document).ready(() => {
     for (let j = 1; j <= 100; j++) {
       let colCod = $(`.colID-${j}`).attr("id").split("-")[1];
       row.append(
-        `<div class="input_cell row-${i}-col-${j}" contenteditable="false" data="colCod-${colCod}"></div>`
+        `<div class="input_cell" id="row-${i}-col-${j}" contenteditable="false" data="colCod-${colCod}"></div>`
       );
     }
     $(".input_container").append(row);
@@ -43,12 +43,69 @@ $(document).ready(() => {
     $(this).toggleClass("selected");
   });
 
-  //Input cell selected Jquery
+  //Input cell selected and multiple selected  Jquery
+  //if ctrl is clicked then multiple select
+  // Otherwise normal select
 
-  $(".input_cell").click(function () {
-    console.log("I am clicked");
-    $(".input_cell.selected").removeClass("selected");
-    $(this).addClass("selected");
+  $(".input_cell").click(function (e) {
+    console.log("Cell clicked");
+    // console.log(e);
+    if (e.ctrlKey) {
+      $(this).addClass("selected");
+      let [rowID, colID] = clicked_row_col(this);
+
+      //checking right cell
+      if (colID < 100) {
+        let right_cell_selected = $(`#row-${rowID}-col-${colID + 1}`).hasClass(
+          "selected"
+        );
+        if (right_cell_selected) {
+          $(this).addClass("right_cell_selected");
+          $(`#row-${rowID}-col-${colID + 1}`).addClass("left_cell_selected");
+        }
+      }
+      if (rowID < 100) {
+        //checking bottom cell
+        let bottom_cell_selected = $(`#row-${rowID + 1}-col-${colID}`).hasClass(
+          "selected"
+        );
+        if (bottom_cell_selected) {
+          $(this).addClass("bottom_cell_selected");
+          $(`#row-${rowID + 1}-col-${colID}`).addClass("top_cell_selected");
+        }
+      }
+
+      //Checking Top cell
+
+      if (rowID > 1) {
+        let top_cell_selected = $(`#row-${rowID - 1}-col-${colID}`).hasClass(
+          "selected"
+        );
+        if (top_cell_selected) {
+          $(this).addClass("top_cell_selected");
+          $(`#row-${rowID - 1}-col-${colID}`).addClass("bottom_cell_selected");
+        }
+      }
+
+      //Checking left cell
+
+      if (colID > 1) {
+        let left_cell_selected = $(`#row-${rowID}-col-${colID - 1}`).hasClass(
+          "selected"
+        );
+        if (left_cell_selected) {
+          $(this).addClass("left_cell_selected");
+          $(`#row-${rowID}-col-${colID - 1}`).addClass("right_cell_selected");
+        }
+      }
+    } else {
+      $(".input_cell.selected").removeClass("top_cell_selected");
+      $(".input_cell.selected").removeClass("bottom_cell_selected");
+      $(".input_cell.selected").removeClass("right_cell_selected");
+      $(".input_cell.selected").removeClass("left_cell_selected");
+      $(".input_cell.selected").removeClass("selected");
+      $(this).addClass("selected");
+    }
   });
 
   //Making cell selectable on DOUBLE click
@@ -61,12 +118,21 @@ $(document).ready(() => {
 
   //Making column and row scrabble with input container scroll
   $(".input_container").scroll(function () {
-    console.log(this.scrollLeft); //This will give how many px we have scroll in horizontal direction
+    //console.log(this.scrollLeft); //This will give how many px we have scroll in horizontal direction
     //The pixel we are scrolling column name
     $(".column_name_container").scrollLeft(this.scrollLeft);
-    console.log(this.scrollTop);
+    // console.log(this.scrollTop);
     $(".row_name_container").scrollTop(this.scrollTop);
 
     //interisting thing i have learnt today
   });
 });
+
+//Utility Functions
+
+function clicked_row_col(cell) {
+  let cellInfo = $(cell).attr("id").split("-");
+  let rowID = parseInt(cellInfo[1]);
+  let colID = parseInt(cellInfo[3]);
+  return [rowID, colID];
+}
