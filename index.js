69eb8980c7dfd53fbfe7457e64876321d3db207d
tarun <tk4977163@gmail.com>
2021-06-22 15:50:29 +0530

    Scrolling of row and column and input cell is added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Using Jquery for Dealing with DOM
 
 $(document).ready(() => {
-  //For column name code and row
+  //For column name code and row name
   for (let i = 1; i <= 100; i++) {
     let ans = "";
     let n = i;
@@ -18,20 +18,22 @@ $(document).ready(() => {
       `<div class="row_name rowID-${i}" id="rowCod-${i}">${i}</div>`
     );
   }
-  //Adding cells
+
+  //Adding cells in input container
 
   for (let i = 1; i <= 100; i++) {
     let row = $(`<div class="cell_row"></div>`);
     for (let j = 1; j <= 100; j++) {
       let colCod = $(`.colID-${j}`).attr("id").split("-")[1];
       row.append(
-        `<div class="input-cell row-${i}-col-${j}" contenteditable="true" data="colCod-${colCod}"></div>`
+        `<div class="input_cell row-${i}-col-${j}" contenteditable="false" data="colCod-${colCod}"></div>`
       );
     }
     $(".input_container").append(row);
   }
 
-  //SELECTED in menu bar
+  //menu icon selected jquery
+
   $(".menu-icon.align-icon").click(function () {
     $(".menu-icon.align-icon.selected").removeClass("selected"); //Removing class form the div which already have
     $(this).addClass("selected"); //Adding class of to the clicked align icon
@@ -41,12 +43,30 @@ $(document).ready(() => {
     $(this).toggleClass("selected");
   });
 
-  //Selected Input cell
+  //Input cell selected Jquery
 
-  $(".input-cell").click(function () {
+  $(".input_cell").click(function () {
     console.log("I am clicked");
-
-    $(".input-cell.selected").removeClass("selected");
+    $(".input_cell.selected").removeClass("selected");
     $(this).addClass("selected");
+  });
+
+  //Making cell selectable on DOUBLE click
+  $(".input_cell").dblclick(function () {
+    console.log("Double clicked");
+
+    $(this).attr("contenteditable", "true");
+    $(this).focus();
+  });
+
+  //Making column and row scrabble with input container scroll
+  $(".input_container").scroll(function () {
+    console.log(this.scrollLeft); //This will give how many px we have scroll in horizontal direction
+    //The pixel we are scrolling column name
+    $(".column_name_container").scrollLeft(this.scrollLeft);
+    console.log(this.scrollTop);
+    $(".row_name_container").scrollTop(this.scrollTop);
+
+    //interisting thing i have learnt today
   });
 });
